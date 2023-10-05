db3ca69e27d65292d586ddfc60f490f5bbf21a6d
Okay-michael <okomichaelakpu@gmail.com>
2023-10-05 11:52:00 -0700

    Printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* printing with puts instead of printf */
+/**
+ * main - Print using puts
+ * Return: Sucessful exit
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
