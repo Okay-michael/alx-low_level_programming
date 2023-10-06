41a6a2d6d4e59e0f03c1e15c4a4488c2185d2201
Ok-michael <okomichaelakpu2@gmail.com>
2023-10-06 22:25:45 +0100

    create 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the program
+ *
+ * Return: this function returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int k = n % 10;
+
+	if ( k > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, k);
+	else if (k = 0)
+		printf("Last digit of %d is %d and is 0", n, k);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, k);
+	return (0);
+}
