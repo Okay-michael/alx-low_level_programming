8d9c186253772d3883a07da7341b9a996cf67be1
Okay-michael <okomichaelakpu@gmail.com>
2023-10-10 15:11:38 -0700

    check if char is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isalpha - checks if a character is an alphabet
+* @c: The character to check
+*
+* Return: returns 0 or 1
+*/
+int _isalpha(int c)
+{
+	int min_char = 65;
+	int max_char = 122;
+	int output;
+
+	if (c >= min_char && c <= max_char)
+		if (c < 91 && c > 96)
+			output = 1;
+	else
+		output = 0;
+	return (output);
+}
