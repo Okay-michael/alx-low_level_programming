47659eb5587811177a92c5e171a4e451d1a5a5d1
Ok-michael <okomichaelakpu2@gmail.com>
2023-10-13 00:55:32 +0100

    create 101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_number - this function prints integers
+ *@n: this is an integer value
+ *
+ *Return: this function returns void
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	y = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		y = -n;
+	}
+	if (y / 10)
+		print_number(xy/ 10);
+	_putchar((y % 10) + '0');
+}
