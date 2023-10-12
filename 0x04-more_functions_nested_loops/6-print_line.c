fd7344eccdd4df1c2fae675db0bc635f776e56aa
Okay-michael <okomichaelakpu@gmail.com>
2023-10-12 12:37:02 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line
+*
+* @n: number of time _ is used
+*
+* Return: void
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n >= 1)
+		{
+			_putchar('-');
+		}
+		else
+		{
+			_putchar('\n');
+			break;
+		}
+	}
+	_putchar('\n')
+}
