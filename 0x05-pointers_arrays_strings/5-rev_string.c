56b836f2c9f8698226b73ce4b4af36d166bd18b3
Ok-michael <okomichaelakpu2@gmail.com>
2023-10-16 23:20:05 +0100

    fix t-re_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,22 +10,10 @@
 void rev_string(char *s)
 {
 	int length = 0;
-	int i = 0;
-	int j;
-	int k;
-	char *s1;
-	
+
 	while (s[length] != '\0')
-	{
 		length++;
-	}
-	for (j = length - 1; j >= 0; j--)
-	{
-		s1[j] = s[i];
-		i++;
-	}
-	for (k = 0; k < length; k++)
-	{
-		s[k] = s1[k];
-	}
+
+	while(length-- >= 0)
+		_putchar(s[length]);
 }
