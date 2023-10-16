fa11935585f8175c5fff9e71834693a7b600723a
Okay-michael <okomichaelakpu@gmail.com>
2023-10-16 13:55:20 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf(*a[i]);
+		printf("%d", a[i]);
 		while (i != n - 1)
 		{
-			printf(', ');
+			printf(", ");
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
