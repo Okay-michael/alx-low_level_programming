701423b164122c0b64cbe47b7e398d9741a901cd
Ok-michael <okomichaelakpu@gmail.com>
2023-10-31 16:53:01 +0100

    create README.md, 0-create_array, main.h files

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - creates an array and fills it with a constant byte
+ * @size: this is the the number of bytes to be created
+ * @c: this is the constant byte to fill the array with
+ * Return: this function returns a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *pter = NULL;
+
+	if (size <= 0)
+		return NULL;
+
+	char *pter = (char*)(malloc(size));
+
+	for(; i < size;)
+		pter[i] = c;
+	return (pter);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+# Malloc and free
+
+this directory demonstrates the use of malloc and
+free for dynamic memory allocation
