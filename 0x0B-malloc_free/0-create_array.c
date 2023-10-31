d551cd6b8ba48bed2e2b983be5f6f19b2a03eac5
Ok-michael <okomichaelakpu@gmail.com>
2023-10-31 17:21:34 +0100

    hotfix on 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	pter = (malloc(size));
 	if (pter == NULL)
 		return NULL;
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		pter[i] = c;
 	return (pter);
 }
