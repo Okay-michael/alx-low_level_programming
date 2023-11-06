f2a2e23c8ae0bb03541b683c4cd619d936d3de99
Ok-michael <okomichaelakpu@gmail.com>
2023-11-06 18:28:29 +0100

    fix a requirement on 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,14 +3,16 @@
 /**
  * print_dog - prints out the fields of a struct dog
  * @d: this is a pointer the struct dog variable
- * @Return: void
  */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nill)");
-		printf("Age: %.6f\n", d->age);
+		if(d->age)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: (nil)");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
