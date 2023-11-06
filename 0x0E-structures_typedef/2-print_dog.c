1593361de77493b33ce7b42f6a65fe9ce5729647
Ok-michael <okomichaelakpu@gmail.com>
2023-11-06 18:31:28 +0100

    fix a requirement on 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nill)");
 		if (d->age)
-			printf("Age: %.6f\n", d->age);
+			printf("Age: %.7f\n", d->age);
 		else
 			printf("Age: (nil)");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
