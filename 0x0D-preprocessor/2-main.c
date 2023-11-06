e754890dd8a386fd452a8742da5456c9fbd9cddf
Ok-michael <okomichaelakpu@gmail.com>
2023-11-06 19:50:38 +0100

    create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the current file name followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
